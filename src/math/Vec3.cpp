bce22269a8119a321f20e23bd4d8ab1e936be921
Veltman Jr <161249791+VokeyJ7@users.noreply.github.com>
2026-06-02 13:07:11 -0500

    Refactor matrix multiplication and clean up code

--- src/math/Vec3.cpp
+++ src/math/Vec3.cpp
@@ -28,7 +28,7 @@ Vector3 crossProduct( Vector3  vec3a, Vector3 vec3b ){
 }
 Vector3 matrix_vec3_Multiplication( Matrix3 mat3a, Vector3 vec3a){
     Vector3 result;
-    result.x = mat3a.m[0][0] * vec3a.x + mat3a.m[0][1] * vec3a.y + mat3a.m[0][2] * vec3a.z; // the argument inherits the m as a method use m to access precise matrix coordinates
+    result.x = mat3a.m[0][0] * vec3a.x + mat3a.m[0][1] * vec3a.y + mat3a.m[0][2] * vec3a.z;
     result.y = mat3a.m[1][0] * vec3a.x + mat3a.m[1][1] * vec3a.y + mat3a.m[1][2] * vec3a.z; 
     result.z = mat3a.m[2][0] * vec3a.x + mat3a.m[2][1] * vec3a.y + mat3a.m[2][2] * vec3a.z;
     return result;
@@ -36,10 +36,10 @@ Vector3 matrix_vec3_Multiplication( Matrix3 mat3a, Vector3 vec3a){
 Matrix3 matrixMultiplication(Matrix3 (&mat3a), Matrix3 (&mat3b)){
     Matrix3 result = {}; // initializes a 0 matrix
 
-    for (int i = 0; i < 3; i++) // all rows
+    for (int i = 0; i < 3; i++) 
     {
-        for (int j=0; j < 3; j++){ // all cols
-            for (int k = 0; k < 3; k++) // 3rd loops walks thru entire matrix (all cells in matrix)
+        for (int j=0; j < 3; j++){ 
+            for (int k = 0; k < 3; k++) 
             {
                 result.m[i][j] += mat3a.m[i][k] * mat3b.m[k][j];
             }
@@ -100,11 +100,7 @@ Vector3 unit(Vector3 (&vec3)){
 
 
 std::vector<Vector3> compute_R(Vector3 v, Vector3 r, Vector3 a, double dt, int steps) { 
-    // this is called euler integration, you take your current velocity
-    // and add it to acceleration multiplied by a slice of time, you get a new velocity 
-    // in the next iteration the new velocity becomes your old velocity which is added to a * dt to make a newer velocity
-    // this process is done over thousands of iterations to get approximations of velocity or position over time
-    std::vector<Vector3> positions; // now it's 3d and can inherit the .push_back func
+    std::vector<Vector3> positions; 
     for (int i = 0; i < steps; i++)
     {
         
@@ -142,8 +138,8 @@ Vector3 compute_F_Net(double T, Vector3 u, double m, Vector3 r, Vector3 v, doubl
     else
     {
         double mag_cube = pow(mag, 3);
-        double c1 = (-1 * m_planet * m * G) / mag_cube; // c is short for constant
-        // I did this to simplify calculations and make arguments look cleaner for scalar multiplication
+        double c1 = (-1 * m_planet * m * G) 
+
         
         fg = scalarMultiplication(r, c1);
 
@@ -178,10 +174,6 @@ double mag_quaternion(Quaternion axis){
     return sqrt(pow(axis.x, 2) + pow(axis.y, 2) + pow(axis.z, 2) + pow(axis.w, 2));
 }
 
-// result.w = w1*w2 - x1*x2 - y1*y2 - z1*z2
-// result.x = w1*x2 + x1*w2 + y1*z2 - z1*y2
-// result.y = w1*y2 - x1*z2 + y1*w2 + z1*x2
-// result.z = w1*z2 + x1*y2 - y1*x2 + z1*w2
 
 Quaternion quaternion_multiplication(Quaternion ax1, Quaternion ax2){
     Quaternion result;
@@ -197,6 +189,7 @@ Quaternion quaternion_multiplication(Quaternion ax1, Quaternion ax2){
 Quaternion conjugate(Quaternion axis){
     return {axis.w, -axis.x, -axis.y, -axis.z};
 }
+
 Quaternion normalize(Quaternion axis){
    double mag = mag_quaternion(axis);
    axis.w = axis.w / mag;
@@ -206,7 +199,7 @@ Quaternion normalize(Quaternion axis){
    return axis;
 }
 
-Vector3 rotateVector( Vector3 vec3a, Quaternion axis){// v' = q*v*q_conjugate. yes order matters.
+Vector3 rotateVector( Vector3 vec3a, Quaternion axis){
     Vector3 v_prime;
     Quaternion v_quat;
     v_quat.w = 0.0;
@@ -244,40 +237,6 @@ Vector3 computeAngularAcceleration(Matrix3 inertia, Vector3 torque){
 
 
 
-// ============================================================
-// ROTATIONAL EQUATIONS OF MOTION
-// ============================================================
-// 
-// STEP 1 - invert inertia tensor (diagonal matrix only)
-// I_inv[0][0] = 1 / I[0][0]
-// I_inv[1][1] = 1 / I[1][1]
-// I_inv[2][2] = 1 / I[2][2]
-// all other cells = 0
-//
-// STEP 2 - compute angular acceleration
-// alpha = I_inv * tau
-//
-// STEP 3 - update angular velocity
-// omega = omega + alpha * dt
-//
-// STEP 4 - convert omega to quaternion
-// omega_quat = {w=0, x=omega.x, y=omega.y, z=omega.z}
-//
-// STEP 5 - compute rate of change of orientation
-// q_dot = 0.5 * q * omega_quat
-// q_dot.w = 0.5 * (q * omega_quat).w
-// q_dot.x = 0.5 * (q * omega_quat).x
-// q_dot.y = 0.5 * (q * omega_quat).y
-// q_dot.z = 0.5 * (q * omega_quat).z
-//
-// STEP 6 - integrate orientation forward in time
-// q.w = q.w + q_dot.w * dt
-// q.x = q.x + q_dot.x * dt
-// q.y = q.y + q_dot.y * dt
-// q.z = q.z + q_dot.z * dt
-//
-// STEP 7 - normalize to prevent drift
-// q = normalize(q)
 
 Quaternion quaternionAddition(Quaternion ax1, Quaternion ax2){
     double w = ax1.w + ax2.w;
@@ -288,7 +247,7 @@ Quaternion quaternionAddition(Quaternion ax1, Quaternion ax2){
     return {w, a, b, c};
 }
 
-std::vector<Quaternion> integrateQuaternion(Matrix3 inertia, Vector3 torque,Vector3 omega, Quaternion q, double dt,  int steps){ // euler integration but for quaternions
+std::vector<Quaternion> integrateQuaternion(Matrix3 inertia, Vector3 torque,Vector3 omega, Quaternion q, double dt,  int steps){
     Vector3 alpha = computeAngularAcceleration(inertia, torque);
     std::vector<Quaternion> quaternions_integrated;
     
@@ -312,24 +271,3 @@ std::vector<Quaternion> integrateQuaternion(Matrix3 inertia, Vector3 torque,Vect
     
 }
 
-// compute_R: Vector3 v, Vector3 r, Vector3 a, double dt, int steps
-// Compute_F_Net: double T, Vector3 u, double m, Vector3 r, Vector3 v, double area, double dce
-// integrateQuaternion: Matrix3 inertia, Vector3 torque, Vector3 omega, Quaternion q, double dt,  int steps
-
-// maxThrust(mt), mass(m), dragCoefficient(dce) 
-
-// what I learned:
-// - creating a struct means that arguments will require the same struct type
-// struc& x, struct& y is how you define the arguments that inherit that struct type
-// stuct (&x)[n] can make n multiples of the struct, in this case it can create a vector containing n vectors
-// push_back is he append but only works for objects under the <vector> class
-// you can reference components inside the struct using . notation
-// don't make the argument a const if you're planning to change it
-
-// You can't have a reference as an argument (&vec3a) unless you make the arg const or if the arg is defined outside of the function
-// if you do make it const you must make the function const as well, but if you make that function const, all the functions
-// in the chain of functions that touch that value in the arg must me const as well, the compiler likes to keep things consistent.
-// think of it like this, a predefined variable aka a variable defined outside of the function, is like a good friend you know well
-// you know them so well so you give them a nickname, (&) a reference, however you can't do this to someone you just met
-// aka an arg that was just placed as a parameter of your function, that value in the arg must have been defined before the function
-// for you to give them a reference.
